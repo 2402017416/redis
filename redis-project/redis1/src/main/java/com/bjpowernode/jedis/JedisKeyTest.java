994588dcac5434b88927db1fc468bb66ac03dc5c
sga <2402017416@qq.com>
2022-10-06 20:47:07 +0800

    1

---
+++ redis-project/redis1/src/main/java/com/bjpowernode/jedis/JedisKeyTest.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.jedis;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+public class JedisKeyTest {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.31.128",6379);
+        String ret = jedis.ping();
+        System.out.println(ret);
+        Set<String> keys = jedis.keys("*");
+        for(String key:keys){
+            System.out.println(key);
+        }
+    }
+}
